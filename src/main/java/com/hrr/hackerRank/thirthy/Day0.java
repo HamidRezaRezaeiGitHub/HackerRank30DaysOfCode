131de1e30d57105710d415ac81edeb95f3556e92
HamidRezaRezaeiGitHub <hamidreza74hrr@yahoo.com>
2023-03-12 12:53:23 -0400

    Day 0 - challenges/30-hello-world added

--- src/main/java/com/hrr/hackerRank/thirthy/Day0.java
+++ src/main/java/com/hrr/hackerRank/thirthy/Day0.java
@@ -1,6 +1,15 @@
 package com.hrr.hackerRank.thirthy;
 
+import java.util.Scanner;
+
 public class Day0 {
-
-
+    //    https://www.hackerrank.com/challenges/30-hello-world/problem?isFullScreen=true
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(str);
+    }
+    
 }
